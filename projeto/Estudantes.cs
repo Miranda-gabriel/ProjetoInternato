01a42880a54fdd3d393432c3eecb030f7b626b96
Gabriel Miranda da Silva <128639665+Miranda-gabriel@users.noreply.github.com>
2023-03-23 16:39:03 -0300

    Add files via upload
    Projeto de Internato e quantidade de quartos alugados 
    C#

---
+++ projeto/Estudantes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoInternato
+{
+    class Estudantes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudantes(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ projeto/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoInternato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Estudantes[] vect = new Estudantes[20];
+
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int Q = int.Parse(Console.ReadLine());
+            Console.WriteLine("--------");
+
+            for (int i = 0; i < Q; i++)
+            {
+                Console.WriteLine($"aluguel #{i}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.WriteLine("------");
+                vect[quarto] = new Estudantes(email,nome);
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 20; i++)
+            {
+                if( vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+
+                }
+            }
+
+
+
+        }
+    }
+}
